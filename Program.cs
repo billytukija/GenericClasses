4a200414c3b5ae5d0f5753d12b80aed5484336af
billytukija <billytukija@gmail.com>
2019-06-13 22:31:49 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericClasses
+{
+
+    public class Steack<T>
+    {
+        int Pos = 0;
+        public T[] Itens = new T[50];
+
+        public void Push(T item)
+        {
+            Itens[Pos] = item;
+            Pos++;
+        }
+
+        public Object Pop()
+        {
+            Pos--;
+            return Itens[Pos];
+        }
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s1 = new Steack<string>();
+            s1.Push("Billy");
+            s1.Push("Tukija");
+            s1.Push("Richard");
+
+            var s2 = new Steack<int>();
+            s2.Push(1);
+            s2.Push(2);
+            s2.Push(3);
+
+            var s3 = new Steack<Product>();
+            s3.Push(new Product { Id = 1, Name = "Guaraná", Description = "Bla bla" });
+            s3.Push(new Product { Id = 2, Name = "Cowbell", Description = "Bla bla" });
+            s3.Push(new Product { Id = 3, Name = "Soja", Description = "Bla bla" });
+
+            foreach (var item in s3.Itens)
+                Console.WriteLine("Values : " + item.Id);
+
+            //Console.WriteLine("Value s1 : " + s1);
+            //Console.WriteLine("------------------");
+            //Console.WriteLine("Value s2 : " + s2);
+            //Console.WriteLine("------------------");
+            //Console.WriteLine("Value s1 : " + s3);
+            Console.ReadLine();
+        }
+    }
+}
