350da16a32665a46eb35669682a0a271a0aefbe1
Billy Tukija Richard <billy.tukija@onlineapp.com.br>
2020-03-02 15:56:11 -0300

    Melhorias no uso das classes genêricas

--- Program.cs
+++ Program.cs
@@ -1,60 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GenericClasses
 {
-
-    public class Steack<T>
-    {
-        int Pos = 0;
-        public T[] Itens = new T[50];
-
-        public void Push(T item)
-        {
-            Itens[Pos] = item;
-            Pos++;
-        }
-
-        public Object Pop()
-        {
-            Pos--;
-            return Itens[Pos];
-        }
-    }
-
-    public class Product
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-    }
 
     class Program
     {
         static void Main(string[] args)
         {
-            var s1 = new Steack<string>();
-            s1.Push("Billy");
-            s1.Push("Tukija");
-            s1.Push("Richard");
+            var names = new Stack<string>();
+
+            names.Push("Billy");
+            names.Push("Tukija");
+            names.Push("Grazielly");
+
 
             var s2 = new Steack<int>();
+
             s2.Push(1);
             s2.Push(2);
             s2.Push(3);
 
-            var s3 = new Steack<Product>();
-            s3.Push(new Product { Id = 1, Name = "Guaraná", Description = "Bla bla" });
-            s3.Push(new Product { Id = 2, Name = "Cowbell", Description = "Bla bla" });
-            s3.Push(new Product { Id = 3, Name = "Soja", Description = "Bla bla" });
+            var product = new Steack<Product>();
+            product.Push(new Product { Id = 1, Name = "Guarana", Description = "Sucré produit au Brésil, principalement en amazonie" });
+            product.Push(new Product { Id = 2, Name = "Cowbell", Description = "Lait produit au Congo (Republique Democratique)" });
+            product.Push(new Product { Id = 3, Name = "Fufu", Description = "Aliment d'origine africaine, consomé au Congo (Republique Democratique) et d'autres de la region " });
 
-            foreach (var item in s3.Itens)
+            foreach (var item in product.Itens)
             {
                 if (item != null)
+                {
                     Console.WriteLine("Id : " + item.Id + " - " + "Name : " + item.Name);
+                    Console.WriteLine("Name : " + item.Description);
+                    Console.WriteLine("--------------*******************************---------------------");
+                }
             }
 
             Console.ReadLine();
---
+++ Steack.cs
@@ -0,0 +1,23 @@
+﻿using OfficeOpenXml.FormulaParsing.Excel.Functions.Text;
+using System;
+
+namespace GenericClasses
+{
+    public class Steack<T>
+    {
+        int Pos = 0;
+        public T[] Itens = new T[50];
+
+        public void Push(T item)
+        {
+            Itens[Pos] = item;
+            Pos++;
+        }
+
+        public Object Pop()
+        {
+            Pos--;
+            return Itens[Pos];
+        }
+    }
+}
